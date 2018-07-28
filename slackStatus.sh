ee5b6a1cd55246208c38e3acc799378f2cf0d9b0
guptaabhishek1 <38529736+guptaabhishek1@users.noreply.github.com>
2018-07-28 13:15:46 +0530

    Update slackStatus.sh

--- slackStatus.sh
+++ slackStatus.sh
@@ -5,7 +5,7 @@ COLOR_CODE=$2
 SLACK_HOOK="https://hooks.slack.com/services/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"
 
 # console logging
-echo -e "SLACK MESSAGE: $MESSAGE\nSLACK EMOJI: $EMOJI\nSLACK_CHANNEL: $SLACK_CHANNEL"
+echo -e "SLACK MESSAGE: $MESSAGE\nSLACK EMOJI: $EMOJI\nSLACK_CHANNEL: $SLACK_CHANNEL\n COLOR:$COLOR_CODE"
 
 # send the notification
 curl -v \
