93b9cf2bb20d2c92dbced9eb5e5772578ca046a0
guptaabhishek1 <38529736+guptaabhishek1@users.noreply.github.com>
2018-07-28 20:37:10 +0530

    Update slackStatus.sh

--- slackStatus.sh
+++ slackStatus.sh
@@ -1,9 +1,13 @@
 MESSAGE=$1
+COLOR_CODE=$2
+
+# Changses 
 EMOJI="${2:-gherkin}"
 SLACK_CHANNEL="#test"
-COLOR_CODE=$2
 SLACK_HOOK="https://hooks.slack.com/services/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"
+USERNAME="Jenkins"
 
+# Dont not change anything from here 
 # console logging
 echo -e "SLACK MESSAGE: $MESSAGE\nSLACK EMOJI: $EMOJI\nSLACK_CHANNEL: $SLACK_CHANNEL\n COLOR:$COLOR_CODE"
 
@@ -11,4 +15,4 @@ echo -e "SLACK MESSAGE: $MESSAGE\nSLACK EMOJI: $EMOJI\nSLACK_CHANNEL: $SLACK_CHA
 curl -v \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data '{"channel": "'"$SLACK_CHANNEL"'", "username": "Jenkins", "attachments": [{"color": "'"$COLOR_CODE"'", "text": "'"$MESSAGE"'"}]}' $SLACK_HOOK
+-X POST --data '{"channel": "'"$SLACK_CHANNEL"'", "username": "'"$USERNAME"'", "attachments": [{"color": "'"$COLOR_CODE"'", "text": "'"$MESSAGE"'"}]}' $SLACK_HOOK
