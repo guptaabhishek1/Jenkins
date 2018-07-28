4ed985494c784553909390e0f93a82949b9ffad7
guptaabhishek1 <38529736+guptaabhishek1@users.noreply.github.com>
2018-07-28 13:00:42 +0530

    Update slackStatus.sh

--- slackStatus.sh
+++ slackStatus.sh
@@ -1,6 +1,7 @@
 MESSAGE=$1
 EMOJI="${2:-gherkin}"
 SLACK_CHANNEL="#test"
+COLOR_CODE=$2
 SLACK_HOOK="https://hooks.slack.com/services/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"
 
 # console logging
@@ -10,4 +11,4 @@ echo -e "SLACK MESSAGE: $MESSAGE\nSLACK EMOJI: $EMOJI\nSLACK_CHANNEL: $SLACK_CHA
 curl -v \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data '{"channel": "'"$SLACK_CHANNEL"'", "username": "webhookbot", "text": "'"$MESSAGE"'"}' $SLACK_HOOK
+-X POST --data '{"channel": "'"$SLACK_CHANNEL"'", "username": "Jenkins", "attachments": [{"color": "'"$COLOR_CODE"'", "text": "'"$MESSAGE"'"}]}' $SLACK_HOOK
