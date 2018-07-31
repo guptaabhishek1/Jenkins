1c7f2e5c07041cd10f1d13238eb4405f4aba0756
guptaabhishek1 <38529736+guptaabhishek1@users.noreply.github.com>
2018-07-31 11:27:21 +0530

    Update compareValues.sh

--- compareValues.sh
+++ compareValues.sh
@@ -5,12 +5,40 @@ comparevalue=$2
 
 compare_content()
 {
-	echo $1
-	echo $2
-	cat $1
-	cat $2
 	status="SUCCESS"
+	for line in `cat $1|sed -e "s/, /,/g"`
+	do
+		event=`echo $line|cut -d "," -f1`
+		for cmp_line in `cat $2|sed -e "s/, /,/g"`
+		do
+			cmp_event=`echo $cmp_line|cut -d "," -f1`
+			if [ ${cmp_event} = ${event} ]
+			then
+				original_value=`echo $line|cut -d "," -f2`	
+				compare_value=`echo $cmp_line|cut -d "," -f2`	
+
+				# get the percent diff 
+				if [ $original_value -eq $compare_value ]
+				then
+					continue
+				elif [ $original_value -eq 0 ]
+				then
+					continue
+				else
+					# pass limit is greater then 50%
+					pass_limit_org_value=`expr $original_value / 2`
+
+					if [ $compare_value -lt $pass_limit_org_value ]
+					then
+						status="FAILED"	
+						break
+					fi
+				fi
+			fi
+		done
+	done
+		
 }
 
-compare_content $1 $2
+compare_content ${actualvalue} ${comparevalue}
 echo $status
