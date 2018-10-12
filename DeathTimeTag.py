134e72603e0214e47502f4d41755aa3a8f948f55
widdix123 <38529736+widdix123@users.noreply.github.com>
2018-10-12 11:11:30 +0530

    Update DeathTimeTag.py

--- DeathTimeTag.py
+++ DeathTimeTag.py
@@ -6,16 +6,21 @@ def updateDeathTimeAsg(asgIDs):
     for asg in asgIDs.split(","):
         print asg
 
+def updateDeathTimeInstance(instanceIDs):
+    #client = boto3.client('autoscaling')
+    for instance in instanceIDs.split(","):
+        print instance
+        
 if __name__ == "__main__":
 
     if sys.argv[1] == "None" or sys.argv[1] == "":
-        print "No Instance to tag"
+        print("No Instance tags specified in jenkins parameters")
     else:
-        print sys.argv[1]
-        updateDeathTimeAsg(sys.argv[1])
+        print("Instance tags found in jenkins parameters")
+        updateDeathTimeInstance(sys.argv[1])
     
-    if sys.argv[2] == "None" or sys.argv[1] == "":
-        print "No Autoscaling to tag"
+    if sys.argv[2] == "None" or sys.argv[2] == "":
+        print("No AutoScaling tags specified in jenkins parameters")
     else:
-        print sys.argv[2]
+        print("AutoScaling tags specified in jenkins parameters")
         updateDeathTimeAsg(sys.argv[2])
