a0df85c4fb3f0837ddbf764d17d9d519a5e56fc9
widdix123 <38529736+widdix123@users.noreply.github.com>
2018-10-12 11:29:34 +0530

    Update DeathTimeTag.py

--- DeathTimeTag.py
+++ DeathTimeTag.py
@@ -1,26 +1,61 @@
-#import boto3
-import sys
+import boto3, sys
 
-def updateDeathTimeAsg(asgIDs):
-    #client = boto3.client('autoscaling')
+def updateDeathTimeAsg(asgIDs, deathTime):
+    client = boto3.client('autoscaling')
+    invalid_asg = []
     for asg in asgIDs.split(","):
-        print asg
+        try:
+            response = client.create_or_update_tags(
+                Tags=[
+                    {
+                        'ResourceId': asg,
+                        'ResourceType': 'auto-scaling-group',
+                        'Key': 'DeathTime',
+                        'Value': deathTime,
+                        'PropagateAtLaunch': True
+                    }]
+                )
+        except:
+            invalid_asg.append(asg)
+    return invalid_asg
+            
 
-def updateDeathTimeInstance(instanceIDs):
-    #client = boto3.client('autoscaling')
+def updateDeathTimeInstance(instanceIDs, deathTime):
+    client = boto3.client('ec2')
+    invalid_instances = []
     for instance in instanceIDs.split(","):
-        print instance
+        try:
+            response = client.create_tags(Resources=[instance],
+                Tags=[
+                    {   
+                        'Key': 'DeathTime',
+                        'Value': deathTime
+                    }]
+                )
+        except:
+            invalid_instances.append(instance)
+    return invalid_instances
         
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    
+    print("#################################################")
     if sys.argv[1] == "None" or sys.argv[1] == "":
         print("No Instance tags specified in jenkins parameters")
     else:
         print("Instance tags found in jenkins parameters")
-        updateDeathTimeInstance(sys.argv[1])
+        invalid_Instances = updateDeathTimeInstance(sys.argv[1], sys.argv[3])
     
+    print("#################################################")
     if sys.argv[2] == "None" or sys.argv[2] == "":
         print("No AutoScaling tags specified in jenkins parameters")
     else:
         print("AutoScaling tags specified in jenkins parameters")
-        updateDeathTimeAsg(sys.argv[2])
+        invalid_ASGs = updateDeathTimeAsg(sys.argv[2], sys.argv[3])
+    print("#################################################")
+
+    if invalid_Instances:
+        print invalid_Instances
+
+    if invalid_ASGs:
+        print invalid_ASGs
