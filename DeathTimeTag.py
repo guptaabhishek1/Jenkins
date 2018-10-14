152782235acaad67baccbe9fef4dd9c5f186137f
widdix123 <38529736+widdix123@users.noreply.github.com>
2018-10-14 08:59:44 +0530

    Update DeathTimeTag.py

--- DeathTimeTag.py
+++ DeathTimeTag.py
@@ -1,9 +1,11 @@
-import boto3, sys
+import boto3
+import sys
 
 def updateDeathTimeAsg(asgIDs, deathTime):
     client = boto3.client('autoscaling')
     invalid_asg = []
     for asg in asgIDs.split(","):
+        print asg
         try:
             response = client.create_or_update_tags(
                 Tags=[
@@ -16,6 +18,7 @@ def updateDeathTimeAsg(asgIDs, deathTime):
                     }]
                 )
         except:
+            
             invalid_asg.append(asg)
     return invalid_asg
             
@@ -24,6 +27,7 @@ def updateDeathTimeInstance(instanceIDs, deathTime):
     client = boto3.client('ec2')
     invalid_instances = []
     for instance in instanceIDs.split(","):
+        print instance
         try:
             response = client.create_tags(Resources=[instance],
                 Tags=[
@@ -39,23 +43,35 @@ def updateDeathTimeInstance(instanceIDs, deathTime):
 
 if __name__ == "__main__":
     
+    # try for east region first
     print("#################################################")
     if sys.argv[1] == "None" or sys.argv[1] == "":
         print("No Instance tags specified in jenkins parameters")
     else:
         print("Instance tags found in jenkins parameters")
-        invalid_Instances = updateDeathTimeInstance(sys.argv[1], sys.argv[3])
+        invalid_InstancesEast = updateDeathTimeInstance(sys.argv[1], sys.argv[3])
     
     print("#################################################")
     if sys.argv[2] == "None" or sys.argv[2] == "":
         print("No AutoScaling tags specified in jenkins parameters")
     else:
         print("AutoScaling tags specified in jenkins parameters")
-        invalid_ASGs = updateDeathTimeAsg(sys.argv[2], sys.argv[3])
+        invalid_ASGsEast = updateDeathTimeAsg(sys.argv[2], sys.argv[3])
     print("#################################################")
 
-    if invalid_Instances:
-        print invalid_Instances
+    if invalid_InstancesEast:
+        # Trying for west region for failed instances if any
+        print("Trying for west region for failed instances if any")
+        invalid_InstancesWest = updateDeathTimeInstance(invalid_InstancesEast, sys.argv[3])
 
-    if invalid_ASGs:
-        print invalid_ASGs
+    if invalid_ASGsEast:
+        # Trying for west region for failed instances if any
+        invalid_ASGsWest = updateDeathTimeAsg(invalid_ASGsEast, sys.argv[3])
+
+    if invalid_InstancesWest:
+        print "Sending email for instances not found in both region east and west"
+        print invalid_InstancesWest
+    
+    if invalid_ASGsWest:
+        print "Sending ASG for instances not found in both region east and west"
+        print invalid_ASGsWest
