de5954bc3461e42ec2f355e878c615eb0ffa32c2
widdix123 <38529736+widdix123@users.noreply.github.com>
2018-10-14 11:22:51 +0530

    Update DeathTimeTag.py

--- DeathTimeTag.py
+++ DeathTimeTag.py
@@ -3,7 +3,8 @@ import sys
 
 def updateDeathTimeAsg(asgIDs, deathTime):
     client = boto3.client('autoscaling')
-    invalid_asg = []
+    invalid_asg = ""
+    #invalid_asg = []
     for asg in asgIDs.split(","):
         print asg
         try:
@@ -18,14 +19,18 @@ def updateDeathTimeAsg(asgIDs, deathTime):
                     }]
                 )
         except:
-            
-            invalid_asg.append(asg)
+	    if invalid_asg:
+                invalid_asg = invalid_asg + "," + asg
+            else:
+                invalid_asg = asg
+            #invalid_asg.append(asg)
     return invalid_asg
             
 
 def updateDeathTimeInstance(instanceIDs, deathTime):
     client = boto3.client('ec2')
-    invalid_instances = []
+    invalid_instances = ""
+    #invalid_instances = []
     for instance in instanceIDs.split(","):
         print instance
         try:
@@ -37,12 +42,21 @@ def updateDeathTimeInstance(instanceIDs, deathTime):
                     }]
                 )
         except:
-            invalid_instances.append(instance)
+            #invalid_instances.append(instance)
+            if invalid_instances:
+                invalid_instances = invalid_instances + "," + instance
+            else:
+                invalid_instances = instance
     return invalid_instances
         
 
 if __name__ == "__main__":
     
+    # Assigning null values to prevent failures
+    invalid_InstancesEast = ""
+    invalid_InstancesWest = ""
+    invalid_ASGsEast = ""
+    invalid_ASGsWest = ""
     # try for east region first
     print("#################################################")
     if sys.argv[1] == "None" or sys.argv[1] == "":
@@ -61,17 +75,23 @@ if __name__ == "__main__":
 
     if invalid_InstancesEast:
         # Trying for west region for failed instances if any
-        print("Trying for west region for failed instances if any")
+        print("Trying for west region to check  for failed instances in east")
         invalid_InstancesWest = updateDeathTimeInstance(invalid_InstancesEast, sys.argv[3])
+        print("#################################################")
 
     if invalid_ASGsEast:
         # Trying for west region for failed instances if any
+        print("Trying for west region to check for failed AutoScalingGroup in east")
         invalid_ASGsWest = updateDeathTimeAsg(invalid_ASGsEast, sys.argv[3])
+        print("#################################################")
 
-    if invalid_InstancesWest:
-        print "Sending email for instances not found in both region east and west"
-        print invalid_InstancesWest
-    
+    # To ensure that file is not appended if  invalid_InstancesWest is empty , therefore adding an or conditon 
+    if invalid_InstancesWest or invalid_ASGsWest:
+        emailFile = open("FailedInstanceData","w")
+        emailFile.write("<br><b>Follwing Instances Could not be found in East or West Region:</b></br><br>%s</br>" %invalid_InstancesWest)
+        emailFile.close()
+
     if invalid_ASGsWest:
-        print "Sending ASG for instances not found in both region east and west"
-        print invalid_ASGsWest
+        emailFile = open("FailedInstanceData","a")
+        emailFile.write("<br><b>Following AutoScalingGroup Could not be found in East or West Region:</b></br><br>%s</br>" %invalid_ASGsWest)
+        emailFile.close()
